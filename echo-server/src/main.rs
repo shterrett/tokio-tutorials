75a02abbd7d10e143ff32f4d0f94c44a5066cbba
Stuart Terrett <shterrett@gmail.com>
2017-01-24 16:56:05 -0500

    Echo Server

---
+++ echo-server/src/main.rs
@@ -0,0 +1,73 @@
+extern crate futures;
+extern crate tokio_core;
+extern crate tokio_proto;
+extern crate tokio_service;
+
+use std::io;
+use std::str;
+use tokio_core::io::{ Codec, EasyBuf, Io, Framed };
+use tokio_proto::TcpServer;
+use tokio_proto::pipeline::ServerProto;
+use tokio_service::Service;
+use futures::{ future, Future, BoxFuture };
+
+pub struct LineCodec;
+
+impl Codec for LineCodec {
+    type In = String;
+    type Out = String;
+
+    fn decode(&mut self, buf: &mut EasyBuf) -> io::Result<Option<Self::In>> {
+        if let Some(i) = buf.as_slice().iter().position(|&b| b == b'\n') {
+            let line = buf.drain_to(i);
+            buf.drain_to(1);
+
+            match str::from_utf8(line.as_slice()) {
+                Ok(s) => Ok(Some(s.to_string())),
+                Err(_) => Err(io::Error::new(io::ErrorKind::Other, "invalid UTF-8"))
+            }
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn encode(&mut self, msg: String, buf: &mut Vec<u8>) -> io::Result<()> {
+        buf.extend(msg.as_bytes());
+        buf.push(b'\n');
+        Ok(())
+    }
+}
+
+pub struct LineProto;
+
+impl<T: Io + 'static> ServerProto<T> for LineProto {
+    type Request = String;
+    type Response = String;
+    type Transport = Framed<T, LineCodec>;
+    type BindTransport = Result<Self::Transport, io::Error>;
+
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
+        Ok(io.framed(LineCodec))
+    }
+}
+
+pub struct Echo;
+
+impl Service for Echo {
+    type Request = String;
+    type Response = String;
+    type Error = io::Error;
+    type Future = BoxFuture<Self::Response, Self::Error>;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+        future::ok(req).boxed()
+    }
+}
+
+
+fn main() {
+    let addr = "0.0.0.0:12345".parse().unwrap();
+    let server = TcpServer::new(LineProto, addr);
+
+    server.serve(|| Ok(Echo));
+}
