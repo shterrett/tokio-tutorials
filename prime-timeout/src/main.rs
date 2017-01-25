77982d34a3aa6243c148b8ea4ff2020db9a108cf
Stuart Terrett <shterrett@gmail.com>
2017-01-24 21:35:51 -0500

    Add timeout to future

--- prime-timeout/src/main.rs
+++ prime-timeout/src/main.rs
@@ -1,8 +1,11 @@
 extern crate futures;
 extern crate futures_cpupool;
+extern crate tokio_timer;
 
+use std::time::Duration;
 use futures::Future;
 use futures_cpupool::CpuPool;
+use tokio_timer::Timer;
 
 const BIG_PRIME: u64 = 15485867;
 
@@ -12,20 +15,21 @@ fn is_prime(num: u64) -> bool {
 
 fn main() {
     let pool = CpuPool::new_num_cpus();
+    let timer = Timer::default();
+
+    let timeout = timer.sleep(Duration::from_millis(750)).then(|_| Err(()));
 
     let prime_future = pool.spawn_fn(|| {
         let prime = is_prime(BIG_PRIME);
 
-        println!("Calculated primeness: {}", prime);
         let res: Result<bool, ()> = Ok(prime);
         res
     });
 
-    println!("Created the future!");
+    let winner = timeout.select(prime_future).map(|(win, _)| win);
 
-    if prime_future.wait().unwrap() {
-        println!("Prime!");
-    } else {
-        println!("Not Prime!");
+    match winner.wait() {
+        Ok(primeness) => println!("Prime? {}", primeness),
+        Err(_) => println!("Timed out")
     }
 }
