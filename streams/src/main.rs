792dbc9866e80b19c8c178011300c989661845a3
Stuart Terrett <shterrett@gmail.com>
2017-01-25 21:24:10 -0500

    (Asynchronous) Stream-based server

--- streams/src/main.rs
+++ streams/src/main.rs
@@ -1,6 +1,7 @@
 extern crate futures;
 extern crate tokio_core;
 
+use futures::Future;
 use futures::stream::Stream;
 use tokio_core::reactor::Core;
 use tokio_core::net::TcpListener;
@@ -10,9 +11,12 @@ fn main() {
     let address = "127.0.0.1:3000".parse().unwrap();
     let listener = TcpListener::bind(&address, &core.handle()).unwrap();
 
+    let handle = core.handle();
     let connections = listener.incoming();
     let welcomes = connections.and_then(|(socket, _)| {
-        tokio_core::io::write_all(socket, b"Hello world!\n")
+        let serve_one = tokio_core::io::write_all(socket, b"Hello world!\n").then(|_| Ok(()));
+        handle.spawn(serve_one);
+        Ok(())
     });
     let server = welcomes.for_each(|_| {
         Ok(())
